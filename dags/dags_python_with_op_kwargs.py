60c4333a20baf2f7c3cfe19aef9c8757500fdea2
gwanghoham <gwangho.ham@data-dynamics.io>
2025-08-24 12:54:53 +0900

    op_kwargs 예제dag 업로드

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from common.common_func import regist2
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 8, 24, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    regist2_t1 = PythonOperator(
+        task_id='regist2_t1',
+        python_callable=regist2,
+        op_args=['hjkim','man','kr','seoul'],
+        op_kwargs={'email':'73berserk@naver.com','phone':'010'}
+    )
+    regist2_t1
